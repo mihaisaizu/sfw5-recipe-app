7d833abd9079e457d299a7ed653a627dac256cc3
mihaisaizu <mihaisorinsaizu@gmail.com>
2019-01-02 21:54:58 +0200

    Initial commit

---
+++ src/main/java/com/mihai/recipeapp/RecipeAppApplication.java
@@ -0,0 +1,14 @@
+package com.mihai.recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeAppApplication.class, args);
+    }
+
+}
+
