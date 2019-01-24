26297fc2f5260b53e187a88ae1e2ebfe645d327c
mihaisaizu <mihaisorinsaizu@gmail.com>
2019-01-24 16:25:27 +0200

    fixed transactional import

--- src/test/java/com/mihai/recipeapp/services/RecipeServiceIT.java
+++ src/test/java/com/mihai/recipeapp/services/RecipeServiceIT.java
@@ -10,8 +10,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 import static org.junit.Assert.assertEquals;
 
