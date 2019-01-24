b0f2fc382bf52dee1df4025a866e786cba515ba7
mihaisaizu <mihaisorinsaizu@gmail.com>
2019-01-24 19:56:48 +0200

    adding new recipe to the DB

--- src/main/java/mihai/recipeapp/controllers/RecipeController.java
+++ src/main/java/mihai/recipeapp/controllers/RecipeController.java
@@ -1,10 +1,10 @@
 package mihai.recipeapp.controllers;
 
+import mihai.recipeapp.commands.RecipeCommand;
 import mihai.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -18,8 +18,23 @@ public class RecipeController {
     @RequestMapping("/recipe/show/{id}")
     public String showById(@PathVariable String id, Model model){
 
-        model.addAttribute("recipe", recipeService.findById(new Long(id)));
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 
         return "recipe/show";
     }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedCommand.getId();
+    }
+
+
 }
