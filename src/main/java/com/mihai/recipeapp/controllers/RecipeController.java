46f6d8d712ea6ddc864799128af33f3bb863b768
mihaisaizu <mihaisorinsaizu@gmail.com>
2019-01-24 18:38:54 +0200

    circleCI try

--- src/main/java/com/mihai/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/mihai/recipeapp/controllers/RecipeController.java
@@ -1,13 +1,12 @@
 package com.mihai.recipeapp.controllers;
 
 import com.mihai.recipeapp.services.RecipeService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//@Slf4j
+
 @Controller
 public class RecipeController {
 
@@ -19,9 +18,7 @@ public class RecipeController {
 
     @RequestMapping("/recipe/show/{id}")
     public String showById(@PathVariable String id, Model model){
-
-        model.addAttribute("recipe", recipeService.findById(new Long(id)));
-
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
     }
 }
